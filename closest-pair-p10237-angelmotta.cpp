8a4647265af61a653e7e85463a2bb2362cbd1932
Angel Motta <angelmotta@gmail.com>
2019-09-02 01:09:47 -0500

    float support

--- closest-pair-p10237-angelmotta.cpp
+++ closest-pair-p10237-angelmotta.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-float min_distance;
+double min_distance;
 
-void calculateDistances(int index_origen, int index_destino, int* x, int* y){
-	int x_dist, y_dist;
-	float distance;
+void calculateDistances(int index_origen, int index_destino, double* x, double* y){
+	double x_dist, y_dist;
+	double distance;
 	x_dist = x[index_destino] - x[index_origen];
 	y_dist = y[index_destino] - y[index_origen];
 	distance = sqrt(x_dist*x_dist + y_dist*y_dist);
 	distance = (int)(distance*10000);
-	distance = (float)distance/10000;
+	distance = (double)distance/10000;
 	//cout << distance << "\n";
 	if(distance < min_distance){
 		min_distance = distance;
 	}
 }
 
-void generateCombinations(int number_points, int* x_dots, int* y_dots){
+void generateCombinations(int number_points, double* x_dots, double* y_dots){
 	for(int i=0; i<number_points-1; ++i){
 		int destino = i;
 		for(int j=0; j<number_points-1-i; ++j){
@@ -34,25 +34,26 @@ void generateCombinations(int number_points, int* x_dots, int* y_dots){
 int main(){
 	int number_points;
 	while(scanf("%d",&number_points) == 1){
-		min_distance = 100000;
+		min_distance = 100000.0;
 		if(number_points == 0) break;
-		int x, y, num_distancias;
-		int* x_coordinates = new int[number_points];
-		int* y_coordinates = new int[number_points];
+		int num_distancias;
+		double x, y;
+		double* x_coordinates = new double[number_points];
+		double* y_coordinates = new double[number_points];
 		num_distancias = number_points*(number_points-1)/2;
 		//int* distancias = new int[num_distancias];
 
 		for(int i=0; i<number_points; ++i){
-			scanf("%d %d", &x, &y);
+			scanf("%lf %lf", &x, &y);
 			x_coordinates[i] = x;
 			y_coordinates[i] = y;
 		}
 		generateCombinations(number_points, x_coordinates, y_coordinates);
 		if(min_distance < 10000){
-			cout << min_distance << "\n";
+			printf("%.4lf\n", min_distance);
 		}
 		else{
-			cout << "INFINITY \n";
+			printf("INFINITY \n");
 		}
 		delete[] x_coordinates;
 		delete[] y_coordinates;
